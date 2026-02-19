26f1ae68d812accf994cc0545c62cb07d50705b2
PlesantJerk <139739823+PlesantJerk@users.noreply.github.com>
2026-02-19 02:50:42 -0500

    Updated config file handling

--- aiqa.js
+++ aiqa.js
@@ -1,18 +1,53 @@
+const fsp = require('fs').promises;
 const { Server, ServerConfig } = require('./server.js')
 const { setTimeout: delay } = require('node:timers/promises');
 const readline = require('readline');
 
 const commands = new Map();
-console.log('starting server');
-var host = new Server(new ServerConfig());
 
-commands.set('cd', (args)=>
-    { 
-        if (args === '')
-            console.log('current dir: ', host.root_dir);
-        else
-            host.root_dir = args; 
-    });
+
+(
+    async ()=>
+    {
+        const rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: true});
+        var config_file = await fsp.readFile('./config.json', 'utf-8');
+        const host_config = JSON.parse(config_file);
+        if (host_config.email === '' || host_config.email == null)
+        {   
+            var block =true;     
+            rl.question("please supply a user_id", async (line)=>
+            {
+                host_config.email = line.trim();
+                await fsp.writeFile('./config.json', JSON.stringify(host_config), 'utf-8');
+                block = false;
+            });
+            while(block)
+            {
+                await delay(250);
+            }
+        }
+        console.log('starting server');
+        var host = new Server(host_config);
+        console.log('polling');
+        commands.set('cd', async (args)=>
+            { 
+                if (args === '')
+                    console.log('current dir: ', host.root_dir);
+                else
+                {
+                    host.root_dir = args; 
+                    host_config.root_dir = args;
+                    await fsp.writeFile('./config.json', JSON.stringify(host_config), 'utf-8');
+                }
+            });
+
+        
+        rl.on('line', on_line_entered);
+
+        host.StartPolling().then(()=>console.log('done')).catch((r)=>console.log('error:', r));
+    }
+)();
+
 
 async function on_line_entered(line)
 {
@@ -28,9 +63,5 @@ async function on_line_entered(line)
         console.log('invalid commmand ', command);
 }
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: true});
-rl.on('line', on_line_entered);
 
 
-console.log('polling');
-host.StartPolling().then(()=>console.log('done')).catch((r)=>console.log('error:', r));
